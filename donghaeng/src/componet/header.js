87fd61e6fb1550c8b05947fe1ecf2d2730a334bf
sori9899 <hyun989969@gmail.com>
2023-06-29 11:43:57 +0900

    Create Header

--- donghaeng/src/componet/header.js
+++ donghaeng/src/componet/header.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "./header.css";
 
 function header(){
     return(
-        <div>
-            <span>동행</span>
+        <div className="header">
+            <div className="header-navigation">
+                <span>동행</span>
+                <span>모집</span>
+                <span>동아리</span>
+            </div>
+
+            <div className="header-active">
+                <span>로그인</span>
+                <span>로그아웃</span>
+            </div>
         </div>
     )
 }
